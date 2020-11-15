75f3d1428c6a346f9d539b5c648192435c536588
I3enito <bensiegenthaler@gmail.com>
2020-11-15 16:12:06 +0100

    finish landing page styling

--- pages/about.jsx
+++ pages/about.jsx
@@ -8,19 +8,37 @@ export default function About() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>ben|siegenthaler|work</title>
+        <title>ben|siegenthaler|about</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header className={styles.header} pageTitle="about"></Header>
 
       <main className={styles.main}>
-        <Image
-          src="/images/github-stylized.png"
-          alt="Stylized Portrait"
-          layout="responsive"
-          height={500}
-          width={500}
-        />
+        <div className={styles.leftColumn}>
+          <div className={styles.imageContainer}>
+            <Image
+              className={styles.portrait}
+              src="/images/github-stylized.png"
+              alt="Stylized Portrait"
+              layout="responsive"
+              height={500}
+              width={500}
+              priority
+            />
+          </div>
+        </div>
+        <div className={styles.rightColumn}>
+          <div>
+            <p className={styles.paragraph}>Hey there!</p>
+            <p className={styles.paragraph}>
+              I'm a software engineer currently living and studying in lucerne,
+              swtzerland. I love the openness of the web and enjoy building
+              gaming experiences. I describe myself as a strong and ambitious
+              team player. If not busy with fixing some nasty bugs, you can find
+              me discussing about politics or enjoying the art of music.
+            </p>
+          </div>
+        </div>
       </main>
     </div>
   );
--- pages/index.jsx
+++ pages/index.jsx
@@ -40,7 +40,7 @@ export default function Home() {
         </a>
         <a
           className={classNames(styles.socialLink)}
-          href="www.linkedin.com/in/ben-siegenthaler-6837781a1"
+          href="https://www.linkedin.com/in/ben-siegenthaler-6837781a1"
           target="_blank"
           rel="noreferrer noopener"
         >
@@ -53,17 +53,6 @@ export default function Home() {
           ></Image>
         </a>
       </footer>
-
-      {/* <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer> */}
     </div>
   );
 }
